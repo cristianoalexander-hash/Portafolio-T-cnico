abeee512e8bfd6ba6c64002ce2d25f0ad8d645f7
cristianoalexander-hash <cristiano.alexander@gmail.com>
2026-09-16 19:55:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Portafolio Técnico - SENCE / Alkemy
+
+window.addEventListener("load", () => {
+    document.body.style.opacity = "1";
+});
+
+document.body.style.opacity = "0";
+document.body.style.transition = "opacity 1s ease-in-out";
+
+// Mensaje en la consola
+console.log("Portafolio Técnico cargado correctamente.");
